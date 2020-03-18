c8ffa14d80605a19f8cf3cce1c767f96957e50df
ljedrejko <61837576+ljedrejko@users.noreply.github.com>
2020-03-18 23:30:17 +0100

    Create main.cpp

---
+++ Projekt_1/solutions/Etap1_ljedrejko/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include "MinesweeperBoard.h"
+
+int main() {
+    srand (time(nullptr));
+
+    MinesweeperBoard Board;
+
+    Board.debug_display();
+    Board.setField_Rand();
+    Board.debug_display();
+    return 0;
+}
